370dc2beb5747e2fcb36331e5807cdecaae8efd0
kamnab <k.kamnab@gmail.com>
2024-10-30 00:52:49 +0700

    add joinTenant

--- src/components/classItem/ClassList.vue
+++ src/components/classItem/ClassList.vue
@@ -5,6 +5,7 @@ import classList from '@/data/classList';
 import axios from 'axios';
 import { loggedInUser } from '@/services/authService';
 import { ref, onMounted } from 'vue';
+import Join from '../tenants/Join.vue';
 
 const user = ref(null);
 const tenants = ref([]);
@@ -49,9 +50,17 @@ onMounted(() => {
     <div class="col-12">
       <div class="d-flex align-items-center justify-content-between flex-nowrap text-nowrap overflow-auto py-1">
         <h1 class="fw-bold pt-6 m-0">Project 2: Backend </h1>
-        <router-link :to="{ name: 'tenant-create' }" class="btn btn-primary btn-sm" v-if="user">
-          Add New
-        </router-link>
+        <div>
+          <router-link :to="{ name: 'tenant-create' }" class="btn btn-primary btn-sm mx-2" v-if="user">
+            Add New
+          </router-link>
+
+          <a href="#" class="btn btn-primary btn-sm" v-if="user" data-bs-toggle="modal"
+            data-bs-target="#modal_join_tenant">
+            Join
+          </a>
+        </div>
+
       </div>
     </div>
     <ClassItemV2 v-for="(item, index) in tenants" :item="item" :key="index"></ClassItemV2>
@@ -66,5 +75,40 @@ onMounted(() => {
     <ClassItem v-for="(item, index) in classList" :item="item" :key="index"></ClassItem>
   </div>
   <!--end::Row-->
+
+  <!--begin::Modal - Select Location-->
+  <div class="modal fade" id="modal_join_tenant" data-bs-backdrop="static" tabindex="-1" role="dialog"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Join Tenant</h5>
+          <!--begin::Close-->
+          <div class="btn btn-icon btn-sm btn-active-light-primary ms-2" data-bs-dismiss="modal">
+            <!--begin::Svg Icon | path: icons/duotone/Navigation/Close.svg-->
+            <span class="svg-icon svg-icon-2x">
+              <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px"
+                height="24px" viewBox="0 0 24 24" version="1.1">
+                <g transform="translate(12.000000, 12.000000) rotate(-45.000000) translate(-12.000000, -12.000000) translate(4.000000, 4.000000)"
+                  fill="#000000">
+                  <rect fill="#000000" x="0" y="7" width="16" height="2" rx="1" />
+                  <rect fill="#000000" opacity="0.5"
+                    transform="translate(8.000000, 8.000000) rotate(-270.000000) translate(-8.000000, -8.000000)" x="0"
+                    y="7" width="16" height="2" rx="1" />
+                </g>
+              </svg>
+            </span>
+            <!--end::Svg Icon-->
+          </div>
+          <!--end::Close-->
+        </div>
+        <div class="modal-body pt-2 pb-0">
+          <Join></Join>
+        </div>
+
+      </div>
+    </div>
+  </div>
+  <!--end::Modal - Select Location-->
 
 </template>
---
+++ src/components/tenants/Join.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="mb-6">
+
+        <form @submit.prevent="joinTenant" id="frmJoinTenant" method="post">
+            <div class="d-flex align-items-center justify-content-between flex-nowrap text-nowrap py-1">
+                <div class="flex-fill mb-8 me-5">
+                    <label for="tenant_code" class="form-label">Code</label>
+                    <input class="form-control" type="text" v-model="tenant.code" id="tenant_code" />
+                </div>
+
+                <button class="btn btn-primary" type="submit">
+                    <span role="spinner" class="spinner-border spinner-border-sm d-none" aria-hidden="true"></span>
+                    <span role="status">Join</span>
+                </button>
+
+            </div>
+        </form>
+    </div>
+</template>
+
+<script setup>
+
+import axios from 'axios';
+import { ref } from 'vue';
+import { loggedInUser } from '@/services/authService';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const user = ref(null);
+const tenant = ref({
+    code: ''
+});
+
+const joinTenant = async () => {
+    if (tenant.value.code === '') {
+        alert('Code can not empty.');
+        return;
+    }
+
+    user.value = await loggedInUser();
+
+    const response = await axios.post(`${import.meta.env.VITE_API_BACKEND_ENDPOINT}/tenants/join`, tenant.value, {
+        headers: {
+            Authorization: `Bearer ${user.value.access_token}`,
+        }
+    });
+
+    if (response.status == 200) {
+        console.log(response.data);
+
+        router.push('/'); // 
+    }
+}
+
+</script>
